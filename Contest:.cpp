e7257430e1bb3ce01e9c7665e9fcc6a4e3306ba5
Sudheer-18 <119719131+Sudheer-18@users.noreply.github.com>
2023-10-07 19:11:51 +0530

    #include<iostream> using namespace std; int main() {     int t,a,b;     cin>>t>>a>>b;     if((a*1)+(b*2)>=t)         cout<<"Qualify";     else         cout<<"Not Qualify"; }

---
+++ Contest:.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int t,a,b;
+    cin>>t>>a>>b;
+    if((a*1)+(b*2)>=t)
+        cout<<"Qualify";
+    else
+        cout<<"Not Qualify";
+}
