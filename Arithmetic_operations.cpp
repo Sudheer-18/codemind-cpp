905ac34c7de7689c45f27172a4f59f7969113235
Sudheer-18 <119719131+Sudheer-18@users.noreply.github.com>
2023-10-21 10:40:50 +0530

    #include<iostream> using namespace std; int main() {     int n,m;     cin>>n>>m;     cout<<"Sum:"<<(n+m)<<" ";     cout<<"Difference:"<<(n-m)<<" ";     cout<<"Product:"<<(n*m)<<" ";     cout<<"Quotient:"<<(n/m)<<" ";     cout<<"Remainder:"<<(n%m)<<" "; }

---
+++ Arithmetic_operations.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,m;
+    cin>>n>>m;
+    cout<<"Sum:"<<(n+m)<<"
+";
+    cout<<"Difference:"<<(n-m)<<"
+";
+    cout<<"Product:"<<(n*m)<<"
+";
+    cout<<"Quotient:"<<(n/m)<<"
+";
+    cout<<"Remainder:"<<(n%m)<<"
+";
+}
